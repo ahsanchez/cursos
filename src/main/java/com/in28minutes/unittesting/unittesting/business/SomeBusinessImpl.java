357bc7498059f7a48d05ff5b1428375289a9061d
Ana Hinojosa <ahinojosa@vectoritcgroup.com>
2018-11-22 08:32:42 +0100

    commit

--- src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/business/SomeBusinessImpl.java
@@ -1,30 +1,38 @@
 package com.in28minutes.unittesting.unittesting.business;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 import com.in28minutes.unittesting.unittesting.data.SomeDataService;
 
 public class SomeBusinessImpl {
-	
-	private SomeDataService someDataService;
-	
-	public void setSomeDataService(SomeDataService someDataService) {
-		this.someDataService = someDataService;
-	}
-
-	public int calculateSum(int [] data) {
-		int sum=0;
-		for (int value:data) {
-			sum += value;
-		}
-		return sum;
-	}
-	
-	public int calculateSumUsingDataService() {
-		int sum=0;
-		int data [] = someDataService.retrieveAllData();
-		for (int value:data) {
-			sum += value;
-		}
-		return sum;
-	}
-
+    
+    private SomeDataService someDataService;
+    
+    public void setSomeDataService(
+        SomeDataService someDataService){
+        this.someDataService = someDataService;
+    }
+    
+    public int calculateSum(
+        int[] data){
+        OptionalInt number = Arrays.stream(data).reduce(Integer::sum);
+        
+        int sum = 0;
+        for (int value : data) {
+            
+            sum += value;
+        }
+        return sum;
+    }
+    
+    public int calculateSumUsingDataService(){
+        int sum = 0;
+        int data[] = someDataService.retrieveAllData();
+        for (int value : data) {
+            sum += value;
+        }
+        return sum;
+    }
+    
 }
